d8c8e77165c6ae16cc7b167a320eac906d479cba
Kareem Ahmed <kareem.ahmed09864@gmail.com>
2026-09-06 23:42:53 +0300

    Empty App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import Sidebar from "./components/layout/Sidebar";
-import { BrowserRouter } from "react-router-dom";
-
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Sidebar/>
-    </BrowserRouter>
+
     </>
   )
 }
